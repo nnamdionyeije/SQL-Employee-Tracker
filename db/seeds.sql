31dd09cc0bb6722c54338d9a03fd2c3119dcfdc1
nnamdionyeije <81874532+nnamdionyeije@users.noreply.github.com>
2023-04-17 13:48:54 -0400

    changed seeds setup

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,13 +7,18 @@ INSERT INTO department (name) VALUES ("Sales");
 
 INSERT INTO role (title, salary, department_id) VALUES ("Sales Lead", 100000, 4);
 INSERT INTO role (title, salary, department_id) VALUES ("Salesperson", 60000, 4);
-INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 50000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 200000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Lead Engineer", 250000, 1);
+INSERT INTO role (title, salary, department_id) VALUES ("Software Engineer", 190000, 2);
+INSERT INTO role (title, salary, department_id) VALUES ("Attorney", 150000, 3);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Nnamdi", "Onyeije", 2, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Carter", 3, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Abraham", "Lincoln", 1, NULL);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Kennedy", 4, 3);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jimmy", "Carter", 4, 1);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Nnamdi", "Onyeije", 1, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Lyndon", "Johnson", 3, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Abraham", "Lincoln", 1, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("John", "Kennedy", 4, 2);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Jimmy", "Carter", 2, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("Gerald", "Ford", 5, NULL);
+
 
 
